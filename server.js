f57c0f9f49f35ad802fec7311932b86308379fc8
MaetDol <rndka9@gmail.com>
2021-01-16 05:46:10 +0900

    Separation image save code
    and some try-catch

--- server.js
+++ server.js
@@ -35,23 +35,38 @@ http.createServer( (req, res) => {
   req
     .on('data', d => data.push(d) )
     .on('end', _=> {
-      const image = Buffer.concat( data );
-      const [, filePath] = req.headers['content-disposition'].match(/filename="(.+)"/) || [];
-      if( !filePath ){
-        log('filename not provided');
-        log(req.headers['content-disposition'])
-        res.writeHead( 415 ).end();
+      try {
+        const image = Buffer.concat( data );
+        const [, filePath] = req.headers['content-disposition'].match(/filename="(.+)"/) || [];
+        if( !filePath ){
+          log('filename not provided');
+          log(req.headers['content-disposition'])
+          res.writeHead( 415 ).end();
+          return;
+        }
+        saveImage( filePath, image );
+
+      } catch(e) {
+        res.writeHead( 500 ).end(e);
+        log('Failed while save image ' + e);
         return;
       }
-      const [fileDir, filename] = filePath.split( path.sep );
-      if( !fs.existsSync(path.resolve('./', fileDir)) ){
-        fs.mkdirSync( path.resolve('./', fileDir) );  
-      }
-      fs.appendFileSync( path.resolve('./', fileDir, filename), image, {flag: 'w'});
-
       res.writeHead( 200 ).end();
     });
 
 }).listen( 3000 );
 
 console.log( 'Running' );
+
+function saveImage( filePath, data ){
+  const [fileDir, name] = filePath.split( path.sep );
+  const dir = path.resolve('./', fileDir);
+  if( !fs.existsSync(dir) ){
+    fs.mkdirSync( dir );  
+  }
+  fs.appendFileSync( 
+    path.resolve(dir, name), 
+    image, 
+    {flag: 'w'}
+  );
+}
