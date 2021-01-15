fe7954561dec63f392c5df645cea6d4d0e285218
MaetDol <rndka9@gmail.com>
2021-01-15 19:59:56 +0900

    Separate stored variables into json file

--- client.js
+++ client.js
@@ -1,16 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
-const baseUrl = '/storage/emulated/0/';
-const lastUpdate = new Date('2021-01-13 17:00:00');
-const targets = [
-  'test',
-];
-console.log( targets.map(d => lookupNewFile(d)) );
+const propPath = path.resolve( __dirname, './properties.json' );
+const { 
+  LAST_UPDATE,
+  BASE_URL,
+  targets
+} = readProps( propPath );
+
+targets
+  .map( d => lookupNewFile(path.join( BASE_URL, d )) )
+  .flat()
+  .forEach( send );
+
+setProps({ LAST_UPDATE: new Date(), BASE_URL, targets }, propPath );
 return 0;
 
+function readProps( path ){
+  const propFile = fs.readFileSync( path, 'UTF-8' );
+  if( !propFile ) throw 'properties.json is empty!';
+
+  const props = JSON.parse( propFile );
+  if( !props.targets.length ) throw 'There is no target directories for watching';
+
+  props.LAST_UPDATE = new Date( props.LAST_UPDATE );
+  return props;
+}
+
+function setProps( prop, path ){
+  fs.writeFileSync( path, JSON.stringify(prop, null, 4) );
+}
+
 function isNewer( target ){
-  return lastUpdate < target;
+  return LAST_UPDATE < target;
 }
 
 function send( path ){
