7b73177488c6fc4e32bdffe77c9b6f7d3fa5a639
MaetDol <rndka9@gmail.com>
2021-01-16 03:45:23 +0900

    Implement send function

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const http = require('http');
 
 const propPath = path.resolve( __dirname, './properties.json' );
 const { 
   LAST_UPDATE,
   BASE_URL,
+  SERVER,
   targets
 } = readProps( propPath );
 
@@ -13,7 +15,7 @@ targets
   .flat()
   .forEach( send );
 
-setProps({ LAST_UPDATE: new Date(), BASE_URL, targets }, propPath );
+//setProps({ LAST_UPDATE: new Date(), BASE_URL, targets }, propPath );
 return 0;
 
 function readProps( path ){
@@ -35,8 +37,34 @@ function isNewer( target ){
   return LAST_UPDATE < target;
 }
 
-function send( path ){
-  console.log( path );
+function send( filePath ){
+  const [lastDir, filename] = filePath.split( path.sep ).slice( -2 );
+  console.log( lastDir, filename )
+  const req = http.request({
+    hostname: SERVER,
+    method: 'POST',
+    port: '3000',
+    headers: {
+      'Content-Type': 'image/png',
+      'Content-Disposition': `attachment; filename="${path.join( lastDir, filename )}"`,
+    },
+  }, res => {
+    const data = [];
+    res.on('data', d => data.push(d) );
+    res.on('end', _=> {
+      console.log('Maybe successfuly done');
+      console.log( data );
+    });
+  });
+
+  req.on('error', e => {
+    console.log('Got an error: ', e);
+  });
+
+  const file = fs.readFileSync( filePath );
+  req.write( file );
+  req.end();
+
 }
 
 function lookupNewFile( dir, result=[] ){
