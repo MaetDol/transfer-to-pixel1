236de195d0f03e86a2fd56760231239d61fdeb91
MaetDol <rndka9@gmail.com>
2021-01-16 03:45:45 +0900

    Generate directory if not exists

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 function log( obj ) {
   console.log( JSON.stringify( obj, null, 2) )
@@ -35,14 +36,18 @@ http.createServer( (req, res) => {
     .on('data', d => data.push(d) )
     .on('end', _=> {
       const image = Buffer.concat( data );
-      const [, filename] = req.headers['content-disposition'].match(/filename="(.+)"/) || [];
-      if( !filename ){
+      const [, filePath] = req.headers['content-disposition'].match(/filename="(.+)"/) || [];
+      if( !filePath ){
         log('filename not provided');
         log(req.headers['content-disposition'])
         res.writeHead( 415 ).end();
         return;
       }
-      fs.appendFileSync(`./${filename}`, image, {flag: 'w'});
+      const [fileDir, filename] = filePath.split( path.sep );
+      if( !fs.existsSync(path.resolve('./', fileDir)) ){
+        fs.mkdirSync( path.resolve('./', fileDir) );  
+      }
+      fs.appendFileSync( path.resolve('./', fileDir, filename), image, {flag: 'w'});
 
       res.writeHead( 200 ).end();
     });
