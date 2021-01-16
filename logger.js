7404b50bdb247685ef85184014d9d28551d45275
MaetDol <rndka9@gmail.com>
2021-01-16 13:45:14 +0900

    Stringify only Object and add prefix

--- logger.js
+++ logger.js
@@ -1,6 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function log( obj ){
-  console.log( JSON.stringify(obj, null, 2) );
+// TODO: write like 16.log file by each date
+module.exports = {
+  info: obj => {
+    let content = stringify( obj );
+    write( '[INFO] ' + content );
+  },
+  err: obj => {
+    const content = stringify( obj );
+    write( '[ERROR]' + content );
+  }
 };
+
+function write( str ) {
+  console.log( str );
+}
+
+function stringify( input ){
+  if( typeof input === 'object' ) {
+    return JSON.stringify( input, null, 2 );
+  }
+  return input;
+}
