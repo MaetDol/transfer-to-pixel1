b4600d92b89483d705d5b4ffcda4b9211f1b66d6
MaetDol <rndka9@gmail.com>
2021-01-21 12:28:50 +0900

    Add PromisePool for limit send file
    Set to 300MB for now

--- client.js
+++ client.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 const log = require('./logger.js');
+const PromisePool = require('./promisePool.js');
 
+// 300 MB
+const promisePool = new PromisePool( 1024 * 1024 * 300 );
 const propPath = path.resolve( __dirname, './properties.json' );
 const { 
   LAST_UPDATE,
@@ -15,16 +18,15 @@ const {
 log.info('\n\n');
 log.info( new Date() );
 log.info('Start lookup new files');
-const triedFiles = targets
-  .map( d => {
-    let newFiles = [];
-    try {
-      newFiles = lookupNewFile(path.join( BASE_URL, d ));
-    } catch(e){
-      log.err('Failed while lookup new file ' + e);
-    }
-    return newFiles;
-  }).flat();
+const triedFiles = targets.map( d => {
+  let newFiles = [];
+  try {
+    newFiles = lookupNewFile(path.join( BASE_URL, d ));
+  } catch(e){
+    log.err('Failed while lookup new file ' + e);
+  }
+  return newFiles;
+}).flat();
 
 const failedFiles = triedFiles.filter( d => {
   try {
@@ -75,36 +77,43 @@ function send( filePath ){
   const [lastDir, filename] = filePath.split( path.sep ).slice( -2 );
   const ext = path.extname( filename ).slice(1);
   const contentType = contentTypeOf( ext );
+  const headers = {
+    'Content-Type': `${contentType}/${ext}`,
+    'Content-Disposition': `attachment; filename="${path.join( lastDir, filename )}"`,
+  };
 
-  const req = http.request({
-    hostname: SERVER,
-    method: 'POST',
-    port: PORT,
-    headers: {
-      'Content-Type': `${contentType}/${ext}`,
-      'Content-Disposition': `attachment; filename="${path.join( lastDir, filename )}"`,
-    },
-  }, res => {
-    const data = [];
-    res.on('data', d => data.push(d) );
-    res.on('end', _=> {
-      if( res.statusCode !== 200 ){
-        log.err(`Tried ${filename}, but received invalid status code! ${res.statusCode}`);
+  const failLog = `Tried ${filename}, but received invalid status code! ${res.statusCode}`;
+  const file = fs.readFileSync( filePath );
+
+  const size = fs.statSync( filePath ).size;
+  pool.push( _=> asyncRequest(headers, file, failLog), size );
+}
+
+function asyncRequest( header, content, failLog ){
+  return new Promise( resolve => {
+    const req = http.request({
+      hostname: SERVER,
+      method: 'POST',
+      port: PORT,
+      headers: header,
+    }, res => {
+      res.on('data', d => 0 );
+      res.on('end', _=> {
+        if( res.statusCode !== 200 ) log.err( failLog );
+        resolve();
+      });
+    });
+
+    req.on('error', e => {
+      if( e ){
+        console.log(e)
+        log.err(`Failed req: ${e}`);
       }
     });
+
+    req.write( content );
+    req.end();
   });
-
-  req.on('error', e => {
-    if( e ){
-      console.log(e)
-      log.err('Got an error: ', e);
-    }
-  });
-
-  const file = fs.readFileSync( filePath );
-  req.write( file );
-  req.end();
-
 }
 
 function lookupNewFile( dir, result=[] ){
