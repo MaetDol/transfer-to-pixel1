a5d5a7cc0e23534fa7c4e7f7a2cba92f5dc50169
Maetdol <rndka9@gmail.com>
2021-01-24 00:22:52 +0900

    Implement filename based on date
    And refactor code

--- logger.js
+++ logger.js
@@ -4,37 +4,60 @@ const path = require('path');
 const {
   LOG_DIR,
   LOGGING,
+  LOG_DISPLAY_ONLY,
+  ROOT,
 } = JSON.parse( fs.readFileSync(
   path.resolve( __dirname, './properties.json' ),
   'UTF-8',
 ));
 
+const BASE = path.resolve( ROOT, LOG_DIR );
+const INFO = 'INFO';
+const ERROR = 'ERROR';
+
 // TODO: write like 16.log file by each date
 module.exports = {
   info: obj => {
-    let content = stringify( obj );
-    write( '[INFO] ' + content );
+    write( `[INFO] ${stringify( obj )}`, INFO );
   },
   err: obj => {
-    const content = stringify( obj );
-    write( '[ERROR] ' + content );
+    write( `[ERROR] ${stringify( obj )}`, ERROR );
   }
 };
 
-function write( str ) {
+function write( str, level ) {
   if( !LOGGING ) return;
 
-  const dirPath = path.resolve(LOG_DIR, './2021');
+  if( LOG_DISPLAY_ONLY === level ){
+    console.log(`${str}\n`);
+    return;
+  }
+
+  const { year, month, date }= now();
+  const dirPath = path.resolve( BASE, `./${year}`);
   if( !fs.existsSync(dirPath) ){
-    fs.mkdirSync( dirPath );
+    fs.mkdirSync( dirPath, { recursive: true });
   }
 
   fs.writeFileSync( 
-    path.resolve( dirPath, './0117.log' ),
+    path.resolve( dirPath, `./${pad(month)}${pad(date)}.log` ),
     str + '\n', 
     {flag:'a'},
   );
   console.log( str );
+}
+
+function pad( v ){
+  return v.toString().padStart(2, 0);
+}
+
+function now(){
+  const date = new Date( new Date().getTime() + 9 * 60*60*1000 );
+  return {
+    year: date.getUTCDate(),
+    month: date.getUTCMonth() + 1,
+    date: date.getUTCDate(),
+  };
 }
 
 function stringify( input ){
