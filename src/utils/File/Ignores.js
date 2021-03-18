2629c6db7c9569be3a7c93fdf8b9d00454c6acbd
MaetDol <rndka9@gmail.com>
2021-03-18 12:38:30 +0900

    Temporary implement includes method
    For test

--- src/utils/File/Ignores.js
+++ src/utils/File/Ignores.js
@@ -1,10 +1,26 @@
+class Pattern {
+  constructor( path ) {
+    this.isDir = path.slice(-1) === '/';
+    this.isRoot = path[0] === '/';
+    this.path = path.split('**');
+  }
+
+  isMatched( target ) {
+    return false;
+  }
+}
+
 class Ignores {
 
   constructor( list ) {
-  
+    this.patterns = list.map( Ignores.parse );
   }
 
   includes( path ) {
+    return this.patterns.some( pattern => pattern.isMatched(path) );
+  }
 
+  static parse( str ) {
+    return new Pattern( str );
   }
 }
