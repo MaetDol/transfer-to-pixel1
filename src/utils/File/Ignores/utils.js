5c7f12d4fbb5a82c64293cbf9861f59301eb4d91
Maetdol <rndka9@gmail.com>
2021-12-28 00:45:10 +0900

    refactor: Ignores, 분리 가능한 함수 별도의 함수로 분리

---
+++ src/utils/File/Ignores/utils.js
@@ -0,0 +1,39 @@
+
+function isDirectory( path ) {
+	return path.slice(-1) === '/';
+}
+
+function convertSeparator( path ) {
+	return path.replace(/\\/g, '/');
+}
+
+const escapeRegExp = part => part.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+const singleAsterisk = part => part.replace(/^\*$/, '[^/]+');
+const doubleAsterisk = part => part.replace(/^\*{2}$/, '([^/]+/?)+');
+const asteriskInText = part => part.replace(
+	/([^*]*)\*([^*]+)|([^*]+)\*([^*]*)/, 
+	(_, group1, group2, group3, group4) => 
+		escapeRegExp(group1 ?? group3) + '[^/]*' + escapeRegExp(group2 ?? group4)
+);
+
+function escape( part ) {
+	const escapeSingleAsterisk = singleAsterisk( part );
+	if( escapeSingleAsterisk !== part ) {
+		return escapeSingleAsterisk;
+	}
+	const escapeDoubleAsterisk = doubleAsterisk( part );
+	if( escapeDoubleAsterisk !== part ) {
+		return escapeDoubleAsterisk;
+	}
+	const escapeAsteriskInText = asteriskInText( part );
+	if( escapeAsteriskInText !== part ) {
+		return escapeAsteriskInText;
+	}
+	return escapeRegExp( part );
+}
+
+module.exports = {
+	isDirectory,
+	convertSeparator,
+	escape,
+};
