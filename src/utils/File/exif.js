fc9fb3726d9ce3ea12c30a4e5f0e2171a9ea5fe1
MaetDol <rndka9@gmail.com>
2023-04-30 14:24:48 +0900

    [#21] exif 수정에 도움을 줄.. Exif 클래스 작성
    지금은 찍은 날짜 가져오기 / 수정 그리고 binary 생성만 추가해뒀어요

--- src/utils/File/exif.js
+++ src/utils/File/exif.js
@@ -1,1 +1,64 @@
-const exifr = require('piexifjs');
+const piexif = require('piexifjs');
+
+const formatter = new Intl.DateTimeFormat('ko-kr', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: false,
+});
+
+const getFormatParts = date => {
+  const rawParts = formatter.formatToParts(date);
+
+  return rawParts.reduce((parts, part) => {
+    parts[part.type] = part.value;
+    return parts;
+  }, {});
+};
+
+const formatToExifDateTime = date => {
+  const { year, month, day, hour, minute, second } = getFormatParts(date);
+
+  return `${year}:${month}:${day} ${hour}:${minute}:${second}`;
+};
+
+class Exif {
+  exif = {
+    '0th': {},
+    Exif: {},
+    GPS: {},
+    Interop: {},
+    '1st': {},
+    thumbnail: null,
+  };
+  jpgBinaryString = '';
+
+  constructor(jpegBinaryString) {
+    this.jpgBinaryString = jpegBinaryString;
+    this.exif = piexif.load(jpegBinaryString);
+  }
+
+  getDateTime() {
+    return (
+      this.exif.Exif[piexif.ExifIFD.DateTimeOriginal] ??
+      this.exif.Exif[piexif.ExifIFD.DateTimeDigitized]
+    );
+  }
+
+  setDateTime(date) {
+    const formattedDate = formatToExifDateTime(date);
+    this.exif.Exif[piexif.ExifIFD.DateTimeOriginal] = formattedDate;
+    this.exif.Exif[piexif.ExifIFD.DateTimeDigitized] = formattedDate;
+  }
+
+  getJpegBinary() {
+    const exifStr = piexif.dump(this.exif);
+    const bin = piexif.remove(this.jpgBinaryString);
+    return Buffer.from(piexif.insert(exifStr, bin), 'binary');
+  }
+}
+
+module.exports = Exif;
--- src/utils/File/index.js
+++ src/utils/File/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   File: require('./File.js'),
   Ignores: require('./Ignores'),
+  Exif: require('./Exif'),
   ...require('./FileUtils.js'),
-  ...require('./exif.js'),
 };
