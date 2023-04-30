03be709b4b44d2cf8567b263570cea50cee93f02
MaetDol <rndka9@gmail.com>
2023-04-30 12:00:09 +0900

    [#21] exifr 은 쓰기가 안되기 때무네 piexifjs 로 변경

---
+++ src/utils/File/exif.js
@@ -0,0 +1,1 @@
+const exifr = require('piexifjs');
--- src/utils/File/index.js
+++ src/utils/File/index.js
@@ -2,4 +2,5 @@ module.exports = {
   File: require('./File.js'),
   Ignores: require('./Ignores'),
   ...require('./FileUtils.js'),
+  ...require('./exif.js'),
 };
