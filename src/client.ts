c413217af23339755d2f4e7076deca0894bfa959
MaetDol <rndka9@gmail.com>
2023-11-18 22:13:31 +0900

    client.ts 에서 env 에 따라 기본 실행여부 변경
    테스트시 import 를 통해 진행하는데, nodejs 특성상 import 한 모듈을 캐싱합니다.
    
    때문에 해당 모듈에서 즉시 실행되는 구문들은, 다시 import 하더라도 실행되지 않는 문제가 있었습니다.
    
    테스트시 활용할 수 있게 함수 형태로 export 하고, env 값에 따라 스크립트 실행시 기본 실행 여부를 달리했습니다

--- src/client.ts
+++ src/client.ts
@@ -8,59 +8,67 @@ import * as log from './utils/logger';
 // @ts-ignore
 import { createRequestFunction, send } from './utils/request';
 
-log.info(new Date());
-const prop = new Properties();
-const {
-  LAST_UPDATE,
-  ROOT,
-  SERVER,
-  PORT,
-  DELETE_AFTER_UPLOAD,
-  targets,
-  ignores: ignorePaths,
-} = prop.value;
+if (process.env.TTP_APP_PRODUCTION) {
+  runClient();
+}
 
-const request = createRequestFunction(SERVER, PORT);
-const ignores = new Ignores(ignorePaths, ROOT);
+export async function runClient() {
+  log.info(new Date());
 
-export const mainPromise = Promise.all(
-  getNewFiles(ROOT, targets, ignores, LAST_UPDATE).map(async (d: string) => {
-    await 0;
+  const prop = new Properties();
+  const {
+    LAST_UPDATE,
+    ROOT,
+    SERVER,
+    PORT,
+    DELETE_AFTER_UPLOAD,
+    targets,
+    ignores: ignorePaths,
+  } = prop.value;
 
-    const file = new File(d);
-    if (!file.mediaType) {
-      log.err(`Not supported file type of ${d}`);
-      return null;
-    }
+  const request = createRequestFunction(SERVER, PORT);
+  const ignores = new Ignores(ignorePaths, ROOT);
 
-    // jpeg EXIF 에 timestamp 가 없을 경우,
-    // File birthtime 으로 exif 를 설정한 복사본 이미지를 만듭니다
-    let isCloned = false;
-    try {
-      if (file.isJpeg() && !hasTimestamp(file)) {
-        isCloned = rewriteTimestamp(file);
+  const results = await Promise.all(
+    getNewFiles(ROOT, targets, ignores, LAST_UPDATE).map(async (d: string) => {
+      await 0;
+
+      const file = new File(d);
+      if (!file.mediaType) {
+        log.err(`Not supported file type of ${d}`);
+        return null;
       }
-    } catch (e) {
-      log.err(`Failed to check exif timestamp while ${file.path}\n\terr: ${e}`);
-    }
 
-    return send(file, DELETE_AFTER_UPLOAD, request)
-      .then((result: number) => {
-        log.info(`Upload is done "${d}"`, true);
-        return result;
-      })
-      .catch((e: unknown) => {
-        log.err(`Failed send file: ${d}, because ${e}`);
-        return file;
-      })
-      .finally(() => isCloned && file.delete());
-  })
-).then(results => {
+      // jpeg EXIF 에 timestamp 가 없을 경우,
+      // File birthtime 으로 exif 를 설정한 복사본 이미지를 만듭니다
+      let isCloned = false;
+      try {
+        if (file.isJpeg() && !hasTimestamp(file)) {
+          isCloned = rewriteTimestamp(file);
+        }
+      } catch (e) {
+        log.err(
+          `Failed to check exif timestamp while ${file.path}\n\terr: ${e}`
+        );
+      }
+
+      return send(file, DELETE_AFTER_UPLOAD, request)
+        .then((result_1: number) => {
+          log.info(`Upload is done "${d}"`, true);
+          return result_1;
+        })
+        .catch((e_1: unknown) => {
+          log.err(`Failed send file: ${d}, because ${e_1}`);
+          return file;
+        })
+        .finally(() => isCloned && file.delete());
+    })
+  );
+
   const faileds = results.filter((v: number | File) => v !== 200);
   log.info(`Tried ${results.length} files, failed ${faileds.length} files.`);
-
   faileds
-    .filter((v: number | File) => v !== null)
+    .filter((v_1: number | File) => v_1 !== null)
     .map(({ path, mode }: File) => {
       fs.chmodSync(path, mode);
     });
@@ -75,7 +83,7 @@ export const mainPromise = Promise.all(
   } else {
     process.exit(0);
   }
-});
+}
 
 function hasTimestamp(file: File) {
   const exif = new Exif(file.read().toString('binary'));
