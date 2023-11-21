d51d6fc4ce4f35ca5f13bb8535601679881c1794
MaetDol <rndka9@gmail.com>
2023-11-21 19:16:43 +0900

    #29 client.test 파일 위치 변경
    테스트하려는 메인 스크립트 근처에 두는 방향으로 진행하려합니다~

--- src/client.test.ts
+++ src/client.test.ts
@@ -8,10 +8,10 @@ import {
 } from '@jest/globals';
 import fs from 'fs';
 import { sep } from 'path';
-import { runClient } from '../src/client';
-import { Exif, File } from '../src/utils/File';
-import { PropertiesJson } from '../src/utils/Properties';
-import { send } from '../src/utils/request';
+import { runClient } from './client';
+import { Exif, File } from './utils/File';
+import { PropertiesJson } from './utils/Properties';
+import { send } from './utils/request';
 
 jest.mock('fs');
 jest.mock('http');
